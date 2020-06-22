62e450edb45f56f09af380e0de7114c2eadcfdac
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-22 14:09:15 -0400

    Completed Homework Assignment

---
+++ src/test/java/com/win/spring_boot_data_assignment/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.win.spring_boot_data_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
